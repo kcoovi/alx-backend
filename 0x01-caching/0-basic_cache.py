4ac766c731406349add0c64b2783545109d926ad
Kevin COOVI <k.coovi@gmail.com>
2024-01-31 23:19:32 +0300

    Caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Cache implementation Class
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    A basic cache implementaion class
+    """
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None and item is not None:
+            self.cache_data.update({key: item})
+
+    def get(self, key):
+        """ Item by key
+        """
+        return self.cache_data.get(key, None)
